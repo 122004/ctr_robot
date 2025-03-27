855823c33763405fefde68c9e183f4f06ce473cc
Wonderfulmello <wonderfulmello1@gmail.com>
2025-03-27 00:36:59 +0000

    motor control update
    it works now

--- CTR_Motor_Control/CTR_Motor_Control.ino
+++ CTR_Motor_Control/CTR_Motor_Control.ino
@@ -11,33 +11,41 @@ int dishpos = 0;
 bool compost = true;
 bool trash = false;
 bool recycling = false;
+int count = 0;
 
 
 void setup() {
   base.attach(9);
   arm.attach(10);
   dish.attach(11);
-
+  
+ 
 }
 
 void loop() {
+
+  //for testing
+  //count = count + 10;
+  //dish.write(count);
+  //delay(1000);
+
   if (compost == true){
     //move to recepticle
-    delay(100);
-    base.write(45);
-    delay(100);
-    arm.write(0);
-    delay(100);
-    dish.write(180);
-    delay(100);
+    delay(3000);
+    base.write(80);
+    delay(3000);
+    arm.write(15);
+    delay(3000);
+    dish.write(0);
+    delay(3000);
     //return to starting position
-    base.write(135);
-    delay(100);
+    dish.write(125);
+    delay(3000);
     arm.write(0);
-    delay(100);
-    dish.write(180);
-    delay(100);
-    compost = false;
+    delay(3000);
+    base.write(115);
+    delay(3000);
+    //compost = false;
     
   }
 
@@ -48,7 +56,7 @@ void loop() {
     delay(100);
     arm.write(0);
     delay(100);
-    dish.write(180);
+    dish.write(0);
     delay(100);
     //return to starting position
     base.write(135);
--- CTR_Motor_Control/build/sketch/CTR_Motor_Control.ino.cpp
+++ CTR_Motor_Control/build/sketch/CTR_Motor_Control.ino.cpp
@@ -13,40 +13,46 @@ int dishpos = 0;
 bool compost = true;
 bool trash = false;
 bool recycling = false;
+int count = 0;
 
 
-#line 16 "C:\\Users\\Owner\\Downloads\\Trash robot\\ctr_robot\\CTR_Motor_Control\\CTR_Motor_Control.ino"
+#line 17 "C:\\Users\\Owner\\Downloads\\Trash robot\\ctr_robot\\CTR_Motor_Control\\CTR_Motor_Control.ino"
 void setup();
 #line 25 "C:\\Users\\Owner\\Downloads\\Trash robot\\ctr_robot\\CTR_Motor_Control\\CTR_Motor_Control.ino"
 void loop();
-#line 16 "C:\\Users\\Owner\\Downloads\\Trash robot\\ctr_robot\\CTR_Motor_Control\\CTR_Motor_Control.ino"
+#line 17 "C:\\Users\\Owner\\Downloads\\Trash robot\\ctr_robot\\CTR_Motor_Control\\CTR_Motor_Control.ino"
 void setup() {
   base.attach(9);
   arm.attach(10);
   dish.attach(11);
-
-
-
+  
+ 
 }
 
 void loop() {
+
+  //for testing
+  //count = count + 10;
+  //dish.write(count);
+  //delay(1000);
+
   if (compost == true){
     //move to recepticle
-    delay(100);
-    base.write(45);
-    delay(100);
-    arm.write(0);
-    delay(100);
-    dish.write(180);
-    delay(100);
+    delay(3000);
+    base.write(80);
+    delay(3000);
+    arm.write(15);
+    delay(3000);
+    dish.write(0);
+    delay(3000);
     //return to starting position
-    base.write(135);
-    delay(100);
+    dish.write(125);
+    delay(3000);
     arm.write(0);
-    delay(100);
-    dish.write(180);
-    delay(100);
-    compost = false;
+    delay(3000);
+    base.write(115);
+    delay(3000);
+    //compost = false;
     
   }
 
@@ -57,7 +63,7 @@ void loop() {
     delay(100);
     arm.write(0);
     delay(100);
-    dish.write(180);
+    dish.write(0);
     delay(100);
     //return to starting position
     base.write(135);
